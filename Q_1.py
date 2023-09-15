d629bf0edeef227e8b20683d0f0b2a8a9543e91f
KoganeRoguen <144063450+KoganeRoguen@users.noreply.github.com>
2023-09-15 15:07:26 -0300

    Q_1.py

--- Q_1.py
+++ Q_1.py
@@ -1,28 +1,4 @@
-import os, random
-
-def ler_valor_inteiro(mensagem):
-    while True:
-        try:
-            return int(input(mensagem))
-        except ValueError:
-            print('\n <Erro> favor, digite um valor inteiro')
-
-def gerar_lista(quantidade, valor_minimo, valor_maximo):
-    if valor_minimo > valor_maximo or quantidade <= 0:
-        return False, None
-    
-    lista_gerada = [random.randint(valor_minimo, valor_maximo) for _ in range(quantidade)]
-    return True, lista_gerada
-
-def salvar_lista(nome_lista, nome_arquivo):
-    try:
-        caminho = os.path.join(os.path.expanduser('~'), 'Documents', nome_arquivo)
-        with open(caminho, 'w') as arquivo:
-            arquivo.writelines(map(lambda x: str(x) + '\n', nome_lista))
-        return True
-    except Exception as e:
-        print(f'\n <Erro> Ocorreu um erro ao salvar a lista: {e}')
-        return False
+from F1 import ler_valor_inteiro, gerar_lista, salvar_lista
 
 v1, v2, v3 = ler_valor_inteiro('Digite o primeiro valor (quantidade): '), ler_valor_inteiro('Digite o segundo valor (valor mínimo): '), ler_valor_inteiro('Digite o terceiro valor (valor máximo): ')
 
