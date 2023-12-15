04e2229fdd433b12b869ae1378322a9705bb6a55
KoganeRoguen <144063450+KoganeRoguen@users.noreply.github.com>
2023-12-15 17:40:45 -0300

    Update 03_udp_echo_model_server.py

--- Documentação Projeto/03_udp_echo_model_server.py
+++ Documentação Projeto/03_udp_echo_model_server.py
@@ -1,32 +1,31 @@
-import socket, subprocess
+import socket
 from socket_constants import *
+from commands import *
 
 print('Recebendo Mensagens...\n\n')
-
-def execute_command(command):
-    try:
-        resultado = subprocess.run(command, shell=True, capture_output=True, text=True)
-        return resultado.stdout
-    except Exception as e:
-        return str(e)
 
 # Criando o socket UDP
 udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-# Ligando o socket à porta
+# Ligando o socket a porta
 udp_socket.bind((HOST_SERVER, SOCKET_PORT))
 
 while True:
     # Recebendo a mensagem e o endereço do cliente
-    data, cliente = udp_socket.recvfrom(BUFFER_SIZE)
-    mensagem = data.decode(CODE_PAGE)
-    print('Conectado por: ', cliente)
-    print('Mensagem recebida: ', mensagem)
+    msg, client = udp_socket.recvfrom(BUFFER_SIZE)
+    print('Conectado por: ', client)
     
-    # Devolvendo uma mensagem (echo) ao cliente
-    mensagem_retorno = execute_command(mensagem)
-    udp_socket.sendto(mensagem_retorno.encode(CODE_PAGE), cliente)
-
-    print('Finalizando Conexão do Cliente ', cliente)
+    # Processando a mensagem e o endereço do cliente
+    try:
+        opt = {'/p' : ping(client[0]),
+               '/s' : showing_ip_configuration()
+               }
+        command = opt[msg.decode(CODE_PAGE)]
+        msg_return = command
+    except Exception as e:
+        msg_return = str(e)
+        
+    # Enviando a resposta de volta para o cliente
+    udp_socket.sendto(msg_return.encode(CODE_PAGE), client)
 
 udp_socket.close()
